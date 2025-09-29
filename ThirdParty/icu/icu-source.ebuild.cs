74f004ae4e4011222540bea676b787f962539583
Ali Efruz Yıldırır <efruzyildirir@gmail.com>
2025-09-29 18:11:42 +0300

    Refactor ICU source and data handling to improve download and extraction process. Added PreBuildSteps

--- ThirdParty/icu/icu-source.ebuild.cs
+++ ThirdParty/icu/icu-source.ebuild.cs
@@ -11,43 +11,68 @@ public class IcuSource : ModuleBase
     {
         this.Type = ModuleType.LibraryLoader;
         this.Name = "icu-source";
-        Console.WriteLine("Looking for ICU sources");
-        if (!ModuleUtilities.GetAndExtractSourceFromArchiveUrl(sourceUrl, Path.Join(context.ModuleDirectory.FullName, "source"), "D5CF533CF70CD49044D89EDA3E74880328EB9426E6FD2B3CC8F9A963D2AD480E"))
-            throw new Exception("Couldn't download ICU sources");
-        string dataBinPath;
+        var sourceDir = Path.Join(context.ModuleDirectory.FullName, "source");
+        if (!Directory.Exists(sourceDir))
+        {
+            this.PreBuildSteps.Add(new ModuleBuildStep("Download ICU sources", (workerType, cancellationToken) =>
+            {
+                if (!ModuleUtilities.GetAndExtractSourceFromArchiveUrl(sourceUrl, sourceDir, "D5CF533CF70CD49044D89EDA3E74880328EB9426E6FD2B3CC8F9A963D2AD480E"))
+                    throw new Exception("Couldn't download ICU sources");
+                return Task.CompletedTask;
+            }));
+        }
+        var dataDir = Path.Join(context.ModuleDirectory.FullName, "data");
+        var hash = string.Empty;
+        var fileName = $"icudt77{(BitConverter.IsLittleEndian ? "l" : "b")}.dat";
+        var dataUrl = string.Empty;
         if (BitConverter.IsLittleEndian)
         {
-            Console.WriteLine("Looking for Little Endian ICU data.");
-            dataBinPath = Path.Join(context.ModuleDirectory.FullName, "data", "little");
-            if (!ModuleUtilities.GetAndExtractSourceFromArchiveUrl(littleEndianDataUrl, dataBinPath, "0913674FF673C585F8BC08370916B6A6CCC30FFB6408A5C1BC3EDBF5A687FD96"))
-                throw new Exception("Couldn't download ICU little endian data.");
-
+            dataDir = Path.Join(dataDir, "little");
+            hash = "0913674FF673C585F8BC08370916B6A6CCC30FFB6408A5C1BC3EDBF5A687FD96";
+            dataUrl = littleEndianDataUrl;
         }
         else
         {
-            Console.WriteLine("Looking for Big Endian ICU data.");
-            dataBinPath = Path.Join(context.ModuleDirectory.FullName, "data", "big");
-            if (!ModuleUtilities.GetAndExtractSourceFromArchiveUrl(bigEndianDataUrl, dataBinPath, "D8BE12E03F782DA350508B15354738ED97A3289008A787B6BD2A85434374BFF4"))
-                throw new Exception("Couldn't download ICU little endian data.");
+            dataDir = Path.Join(dataDir, "big");
+            hash = "D8BE12E03F782DA350508B15354738ED97A3289008A787B6BD2A85434374BFF4";
+            dataUrl = bigEndianDataUrl;
         }
-        var dataFileName = $"icudt77{(BitConverter.IsLittleEndian ? "l" : "b")}.dat";
-        File.Copy(Path.Join(dataBinPath, dataFileName), Path.Join(context.ModuleDirectory.FullName, "source", "icu", "source", "data", "in", dataFileName), true);
+        var filePath = Path.Join(dataDir, fileName);
         var includeDir = Path.Join(context.ModuleDirectory.FullName, "include", "unicode");
-        if (!Directory.Exists(includeDir))
+        var copyToPath = Path.Join(context.ModuleDirectory.FullName, "source", "icu", "source", "data", "in", fileName);
+        // if the data file doesn't exist, download and extract it
+        // if the data file exists but the copy to path doesn't, copy it
+        if (!File.Exists(filePath) || !File.Exists(copyToPath))
         {
-            Directory.CreateDirectory(includeDir);
-            var copyFromDir = Path.Join(context.ModuleDirectory.FullName, "source", "icu", "source", "common", "unicode");
-            foreach (var file in Directory.GetFiles(copyFromDir, "*.h"))
+            this.PreBuildSteps.Add(new ModuleBuildStep("Fetch and prepare ICU Data Zip", (workerType, cancellationToken) =>
             {
-                var destFile = Path.Join(includeDir, Path.GetFileName(file));
-                File.Copy(file, destFile, true);
-            }
-            copyFromDir = Path.Join(context.ModuleDirectory.FullName, "source", "icu", "source", "i18n", "unicode");
-            foreach (var file in Directory.GetFiles(copyFromDir, "*.h"))
-            {
-                var destFile = Path.Join(includeDir, Path.GetFileName(file));
-                File.Copy(file, destFile, true);
-            }
+                if (!ModuleUtilities.GetAndExtractSourceFromArchiveUrl(littleEndianDataUrl, dataDir, hash))
+                    throw new Exception("Couldn't download ICU little endian data.");
+                // copy the data file to the source/data/in directory
+                File.Copy(filePath, copyToPath, true);
+                return Task.CompletedTask;
+            }));
         }
+        if (!File.Exists(includeDir))
+        {
+            this.PreBuildSteps.Add(new ModuleBuildStep("Prepare ICU include files", (workerType, cancellationToken) =>
+            {
+                Directory.CreateDirectory(includeDir);
+                var copyFromDir = Path.Join(context.ModuleDirectory.FullName, "source", "icu", "source", "common", "unicode");
+                foreach (var file in Directory.GetFiles(copyFromDir, "*.h"))
+                {
+                    var destFile = Path.Join(includeDir, Path.GetFileName(file));
+                    File.Copy(file, destFile, true);
+                }
+                copyFromDir = Path.Join(context.ModuleDirectory.FullName, "source", "icu", "source", "i18n", "unicode");
+                foreach (var file in Directory.GetFiles(copyFromDir, "*.h"))
+                {
+                    var destFile = Path.Join(includeDir, Path.GetFileName(file));
+                    File.Copy(file, destFile, true);
+                }
+                return Task.CompletedTask;
+            }));
+        }
+
     }
 }
